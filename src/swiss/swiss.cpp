787496e513769a005a174e26ac156d0cc221c3cf
Rachel Havranek <rachel.havranek@colorado.edu>
2022-05-15 18:35:59 -0600

    Update swiss.cpp
    change the messages to be shorter

--- src/swiss/swiss.cpp
+++ src/swiss/swiss.cpp
@@ -95,15 +95,15 @@ ValcoValveLoggerComponent* valco = new ValcoValveLoggerComponent(
 const SchedulerEvent schedule[] = {
    {30,   SECONDS,      EVENT_START,      "start", "let's do this!"}, //enough time to power up mfc
    {5,    MINUTES,      EVENT_CLEAN,      "flush internal", "flush internal lines"}, 
-   {10,   MINUTES,      EVENT_CLEAN25CM,  "flush 25 probe", "flush the 25 cm probe" },
-   {45,   MINUTES,      EVENT_START_25CM, "flush flask 25cm", "sample at 25cm depth"},
-   {15,   SECONDS,      EVENT_END_25CM,   "finish 25cm", "finished sampling 25"},
-   {10,   MINUTES,      EVENT_CLEAN50CM,  "flush 50 probe", "flush the 50 cm probe" },
-   {45,   MINUTES,      EVENT_START_50CM, "flush 50cm flask", "sample at 50cm"},
-   {15,   SECONDS,      EVENT_END_50CM,   "finish 50cm"},
-   {10,   MINUTES,      EVENT_CLEAN75CM,  "flush 75cm probe", "flush the 75 cm probe" },
-   {45,   MINUTES,      EVENT_START_75CM,  "flush 75 flask", "sample at 75 cm"},
-   {15,   SECONDS,      EVENT_END_75CM,   "finish 75 cm"},
+   {10,   MINUTES,      EVENT_CLEAN25CM,  "flush probe", "flush the 25 cm probe" },
+   {45,   MINUTES,      EVENT_START_25CM, "flush flask", "sample at 25cm depth"},
+   {15,   SECONDS,      EVENT_END_25CM,   "END! 25cm", "finished sampling 25"},
+   {10,   MINUTES,      EVENT_CLEAN50CM,  "flush probe", "flush the 50 cm probe" },
+   {45,   MINUTES,      EVENT_START_50CM, "flush flask", "sample at 50cm"},
+   {15,   SECONDS,      EVENT_END_50CM,   "END! 50cm"},
+   {10,   MINUTES,      EVENT_CLEAN75CM,  "flush probe", "flush the 75 cm probe" },
+   {45,   MINUTES,      EVENT_START_75CM,  "flush flask", "sample at 75 cm"},
+   {15,   SECONDS,      EVENT_END_75CM,   "END! 75 cm"},
    {5,    MINUTES,      EVENT_END_CLEAN,  "final clean"},
    {1,    MINUTES,      EVENT_END,        "complete"}
 };
